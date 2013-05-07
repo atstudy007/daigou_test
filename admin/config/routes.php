c48cb132559569ece7b011e3544492bd8b89bcb0
yonghao <yonghao@mozat.com>
2013-05-07 17:14:31 +0800

    update

--- admin/config/routes.php
+++ admin/config/routes.php
@@ -41,6 +41,8 @@
 $route['default_controller'] = "login";
 $route['404_override'] = '';
 
+$route['login'] = 'login';
+
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
