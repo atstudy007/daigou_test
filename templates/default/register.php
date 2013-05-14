4e990840c5feb84f77132324fa5be5e0f9bef5f8
cyhlk <npucyh@gmail.com>
2013-05-14 22:25:32 +0800

    update

--- templates/default/register.php
+++ templates/default/register.php
@@ -65,6 +65,16 @@
                               <td align="left" valign="middle" ></td>
                             </tr>
                             <tr>
+                              <td align="right" valign="middle" class="title color666">Address</td>
+                              <td colspan="2" valign="middle" class="input"><input  type="text" id="address" name="adress" maxlength="16" value="Your mail address." /></td>
+                              <td align="left" valign="middle" ></td>
+                            </tr>
+                            <tr>
+                              <td align="right" valign="middle" class="title color666">Phone</td>
+                              <td colspan="2" valign="middle" class="input"><input  type="text" id="phone" name="phone" maxlength="16" value="Your phone number." /></td>
+                              <td align="left" valign="middle" ></td>
+                            </tr>
+                            <tr>
                 <td align="right" valign="middle" class="title color666">Captcha</td>
                 <td width="96" valign="middle" class=""><input autocomplete="off" name="captcha" id="captcha" type="text" style="width:70px;" />
 				</td>
