83d7ef27b7eab3c199b0d784c15cb39c4015fc95
cyhlk <npucyh@gmail.com>
2013-05-20 00:18:12 +0800

    update

--- schedule/controllers/cron.php
+++ schedule/controllers/cron.php
@@ -6,15 +6,18 @@ class Cron extends CI_Controller
     {
         //$this->load->library('cron_schedule');
         //$this->cron_schedule->dispatch();
-		$this->load->model('taobao/taobao_email_queue_mdl');
-		$i = 1;
-		while($i < 11)
-		{
-			$this->taobao_email_queue_mdl->init();
-			$this->taobao_email_queue_mdl->auto_send();
-			$i++;
-			sleep(5);
-		}
+//		$this->load->model('taobao/taobao_email_queue_mdl');
+//		$i = 1;
+//		while($i < 11)
+//		{
+//			$this->taobao_email_queue_mdl->init();
+//			$this->taobao_email_queue_mdl->auto_send();
+//			$i++;
+//			sleep(5);
+//		}
+        $this->load->model('taobao/taobao_email_queue_mdl');
+        $this->taobao_email_queue_mdl->init();
+        $this->taobao_email_queue_mdl->auto_send();
     }
 }
  
